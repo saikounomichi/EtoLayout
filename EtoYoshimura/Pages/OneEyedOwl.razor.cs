75cdee09672bf448fe67424be37228d786bd3d0c
saikounomichi <dooby6823@gmail.com>
2026-02-22 11:22:25 +0100

    FEAT: copied over the layout from private repo

---
+++ EtoYoshimura/Pages/OneEyedOwl.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EtoYoshimura.Pages;
+
+public partial class OneEyedOwl : ComponentBase
+{
+}
---
+++ EtoYoshimura/Pages/Personality.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EtoYoshimura.Pages;
+
+public partial class Personality : ComponentBase
+{
+    
+}
